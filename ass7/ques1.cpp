60a4d08ef2700848c4014fca1d7b463ea8acbbf8
mohanarora3 <mohanarora707@gmail.com>
2025-11-21 10:47:58 +0530

    Implement various sorting algorithms in ques1.cpp

---
+++ ass7/ques1.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+class sortingalgo {
+public :
+void bubblesort(vector<int>& arr) {
+    int n = arr.size();
+    for(int i = n-1 ;i >=1; i--) {
+        for(int j = 0; j < i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+void selectionsort(vector<int>& arr) {
+    int n = arr.size();
+    for(int i = 0; i < n-2; i++) {
+        int minindex = i;
+        for(int j = i; j < n-1; j++) {
+            if(arr[j] < arr[minindex]) {
+                minindex = j;
+            }
+        }
+        swap(arr[i], arr[minindex]);
+    }
+}
+
+
+
+void merge(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low, right = mid + 1;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right])
+                temp.push_back(arr[left++]);
+            else
+                temp.push_back(arr[right++]);
+        }
+        while (left <= mid)
+            temp.push_back(arr[left++]);
+        while (right <= high)
+            temp.push_back(arr[right++]);
+        for (int i = low; i <= high; i++)
+            arr[i] = temp[i - low];
+    }
+    void mergeSort(vector<int>& arr, int low, int high) {
+        if (low >= high)
+            return;
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+
+
+void insertionsort(vector<int>& arr){
+    int n = arr.size();
+for (int i =0 ; i <= n-1 ; i++){
+j =i;
+while( j > 0 && a[j-1] > a[j]){
+swap ( a[j-1] , a[j])
+    j--
+    }}
+
+void quickSort(vector<int>& arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        return i + 1;
+    }
+};
